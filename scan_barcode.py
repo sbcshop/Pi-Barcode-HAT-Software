487cb0d0396be0466883c5afe56f4f7e6d6e98fc
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-10-18 11:56:17 +0530

    Update scan_barcode.py

--- scan_barcode.py
+++ scan_barcode.py
@@ -3,7 +3,7 @@ This is the example code for scanning barcode
 Develop by Sb-components
 
 '''
-from PIL import Image,ImageDraw,ImageFont
+from images import Image,ImageDraw,ImageFont
 import zero_barcode
 import time
 import sys
